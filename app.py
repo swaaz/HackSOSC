0ec57ef175825c6da0dfd8ed6cf666ba64d8ab4a
swaaz <swaasthik.shetty07@gmail.com>
2020-06-28 03:52:15 +0530

    installed tensorflow and linked HTML,JS and CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "Testing Flask!!!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
