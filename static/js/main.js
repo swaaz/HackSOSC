4ffeb6d9eff1692a2250324b79bfcc64cddea146
swaaz <swaasthik.shetty07@gmail.com>
2020-06-28 16:20:08 +0530

    initila commit

--- static/js/main.js
+++ static/js/main.js
@@ -8,10 +8,10 @@ ifrm.style="width: 280px; max-height: 100%; height: 338px; position: fixed; bott
 document.body.appendChild(ifrm);
 
 
-//input text
-//function inputText(text){
+// input text
+// function inputText(text){
 //    $('body').append("<div class='chat-container input'><p class='input'>"+text+"</p></div><br>");
-//};
+// };
 
 //output text
 //function outputText(text){
