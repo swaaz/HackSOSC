4b8867bc46d5ab845af75ade752b916f3b353caa
swaaz <swaasthik.shetty07@gmail.com>
2020-06-29 09:56:47 +0530

    testing

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask , render_template, request
-# import  main
+import  main
 app = Flask(__name__)
-# main.run()
+main.run()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    # return "hello"
 
 @app.route("/get")
 #function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
-    return ("Hello Swaaz!!!")
-    # return str(main.chat(userText))
+    # return ("Hello Swaaz!!!")
+    return str(main.chat(userText))
 
 if __name__ == '__main__':
     app.run(debug=True)
